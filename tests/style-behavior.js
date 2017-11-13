3ecdaa84483afe54e60da72bdde6a66454171142
Xenock <miguelmoracho@gmail.com>
2017-11-13 14:19:49 +0100

    testing interactions with select boxes

--- tests/style-behavior.js
+++ tests/style-behavior.js
@@ -4,9 +4,29 @@ module.exports = {
     client.waitForElementPresent('path.leaflet-interactive', 3000)
     client.expect.element('path.leaflet-interactive').to.be.present
     client.getAttribute('path.leaflet-interactive', 'fill', function(result){
-      this.assert.equal(result.value, '#A52A2A')
+      this.assert.equal(result.value, "#A52A2A")
     })
-    client.click('.painting-style__fill option[value=#DEB887]')
+    client.click('#painting-style__fill')
+    client.waitForElementVisible("option[value='#DEB887']", 1000)
+    client.click("option[value='#DEB887']")
+    client.getAttribute('path.leaflet-interactive', 'fill', function(result){
+      this.assert.equal(result.value, '#DEB887')
+    })
+    client.end()
+  },
+  'select changes stroke width': function(client){
+    client.url('http://localhost:9000')
+    client.waitForElementPresent('path.leaflet-interactive', 3000)
+    client.expect.element('path.leaflet-interactive').to.be.present
+    client.getAttribute('path.leaflet-interactive', 'stroke-width', function(result){
+      this.assert.equal(result.value, 1)
+    })
+    client.click('#painting-style__stroke')
+    client.waitForElementVisible('option[value="2"]', 1000)
+    client.click('option[value="2"]')
+    client.getAttribute('path.leaflet-interactive', 'stroke-width', function(result){
+      this.assert.equal(result.value, 2)
+    })
     client.end()
   }
 }
